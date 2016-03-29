a947ddec262aedc5fdb8005d8042443068b7ed83
Jonas Hu <k51015101@gmail.com>
2016-03-29 22:29:34 +0800

    test

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,33 +1,33 @@
-// var mql = window.matchMedia("(max-width: 768px)");
-// var barchart = '<cloudstitch-d3 user="jonashu" app="d3-hierarchical-bar-chart"></cloudstitch-d3>';
-// var bubblechart = '<cloudstitch-d3 user="jonashu" app="d3-bubble-chart"></cloudstitch-d3>';
-//
-// $(document).ready(function(){
-//     if (mql.matches) {
-//         document.getElementById("barchart").innerHTML = barchart;
-//         document.getElementById("cloudstitch").innerHTML='';
-//     } else {
-//         document.getElementById("cloudstitch").innerHTML = bubblechart;
-//         document.getElementById("barchart").innerHTML='';
-//     }
-// })
-//
-// // 指定回调函数
-// mql.addListener(mqCallback);
-//
-// // 撤销回调函数
-// // mql.removeListener(mqCallback);
-//
-// function mqCallback(mql) {
-//     if (mql.matches) {
-//       $(document).ready(function(){
-//         document.getElementById("barchart").innerHTML = barchart;
-//         document.getElementById("cloudstitch").innerHTML='';
-//       })
-//     } else {
-//       $(document).ready(function(){
-//         document.getElementById("cloudstitch").innerHTML = bubblechart;
-//         document.getElementById("barchart").innerHTML='';
-//       })
-//     }
-// }
+var mql = window.matchMedia("(max-width: 768px)");
+var barchart = '<cloudstitch-d3 user="jonashu" app="d3-hierarchical-bar-chart"></cloudstitch-d3>';
+var bubblechart = '<cloudstitch-d3 user="jonashu" app="d3-bubble-chart"></cloudstitch-d3>';
+
+$(document).ready(function(){
+    if (mql.matches) {
+        document.getElementById("barchart").innerHTML = barchart;
+        document.getElementById("cloudstitch").innerHTML='';
+    } else {
+        document.getElementById("cloudstitch").innerHTML = bubblechart;
+        document.getElementById("barchart").innerHTML='';
+    }
+})
+
+// 指定回调函数
+mql.addListener(mqCallback);
+
+// 撤销回调函数
+// mql.removeListener(mqCallback);
+
+function mqCallback(mql) {
+    if (mql.matches) {
+      $(document).ready(function(){
+        document.getElementById("barchart").innerHTML = barchart;
+        document.getElementById("cloudstitch").innerHTML='';
+      })
+    } else {
+      $(document).ready(function(){
+        document.getElementById("cloudstitch").innerHTML = bubblechart;
+        document.getElementById("barchart").innerHTML='';
+      })
+    }
+}
